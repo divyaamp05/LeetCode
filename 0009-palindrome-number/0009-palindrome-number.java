4742fa13e96e3a7fb87e58ff21911d254c10a9aa
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2024-10-31 17:50:54 +0530

    Time: 4 ms (100%), Space: 44 MB (49.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,25 +1,29 @@
 class Solution {
-    public boolean isPalindrome(int x)
-    {
-        int n=Math.abs(x);
-        int rem=0;
-        int res=0;
-        while(n!=0)
-        {
-            rem=n%10;
-            
-            res=(res*10)+rem;
-            n=n/10;
-        }
+    public boolean isPalindrome(int x) {
+        int n = Math.abs(x);
+        int rem = 0;
+        int res = 0;
 
-        if(x==res)
-        {
-            return true;
-        }
-        else
+        if(x<0)
         {
             return false;
         }
-               
+        
+        while (n != 0) 
+        {
+            rem = n % 10;
+            res = (res * 10) + rem;
+            n = n / 10;
+        }
+
+        if (x == res) 
+        {
+            return true;
+        } 
+        else 
+        {
+            return false;
+        }
+
     }
 }
