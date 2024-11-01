c988ec3707951f9952947dae5091b0b17c2ea8e5
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2024-11-01 23:38:57 +0530

    Time: 2 ms (77.72%), Space: 61.5 MB (71.59%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,7 +3,11 @@ class Solution {
         int min = prices[0];
         int cost = 0;
         int profit = 0;
-
+        
+        if(prices.length==0)
+        {
+            return 0;
+        }
         for (int i = 0; i < prices.length; i++) 
         {
             cost = prices[i] - min;
