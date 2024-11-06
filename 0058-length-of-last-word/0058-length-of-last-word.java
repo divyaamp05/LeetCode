92e0d402f03df2e5ebd9a90add5a2a23328365dd
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2024-11-06 11:55:41 +0530

    Time: 0 ms (100%), Space: 41.3 MB (90.16%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,22 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) 
     {
+        s=s.trim();
         int count=0;
-        for (int i = s.length()-1; i >= 0; i--) 
+        for(int i=s.length()-1;i>=0;i--)
         {
-            char ch = s.charAt(i);
-            if (ch != ' ') 
-            {
-                count++;
-            } 
-
-            else if (count != 0) 
+            if(s.charAt(i) == ' ')
             {
                 break;
             }
-
+            else
+            {
+                count++;
+            }
         }
         return count;
-
     }
 }
