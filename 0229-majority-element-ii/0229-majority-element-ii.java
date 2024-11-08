317a8633327ad420041e265b04391188e112d8ff
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2024-11-08 19:48:53 +0530

    Time: 3 ms (75.31%), Space: 48.1 MB (40.16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,30 +1,70 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) 
     {
-       
-       ArrayList <Integer> temp = new ArrayList <> ();
+        int count1=0;
+        int count2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
 
-       for(int i=0;i<nums.length;i++)
+        ArrayList <Integer> temp =new ArrayList <> ();
+
+        for(int i=0;i<nums.length;i++)
         {
-            if(temp.size()==0 || !( temp.contains(nums[i]) ) )
+            if(count1==0 && nums[i]!=ele2)
             {
-                int count=0;
-                for(int j=0;j<nums.length;j++)
-                {
-                    if(nums[i]==nums[j])
-                    {
-                    count++;
-                    }
-                }
-
-                if(count > Math.floor(nums.length/3))
-                {
-                    temp.add(nums[i]);
-                }  
+                ele1=nums[i];
+                count1=1;
             }
-          
-        } 
 
-        return temp;
+            else if(count2==0 && nums[i]!=ele1)
+            {
+                ele2=nums[i];
+                count2=1;
+            }
+
+            else if(ele1== nums[i])
+            {
+                count1++;
+            }
+
+            else if(ele2== nums[i])
+            {
+                count2++;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1=0;
+        count2=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==ele1)
+            {
+                count1++;
+            }
+            if(nums[i]==ele2)
+            {
+                count2++;
+            }
+        }
+
+        if(count1> Math.floor(nums.length/3))
+        {
+            temp.add(ele1);
+        }
+
+        if(count2> Math.floor(nums.length/3))
+        {
+            temp.add(ele2);
+        }
+         
+
+        return temp; 
+
     }
 }
