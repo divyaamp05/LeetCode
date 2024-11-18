bfe00a3f46a6655ac8da4e090235eb3eb765eb82
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2024-11-18 14:07:57 +0530

    Time: 1 ms (86.66%), Space: 40.7 MB (71.96%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int mySqrt(int x) 
+    {
+        int low=1;
+        int high =x;
+        int ans=0;
+
+        while(low<=high)
+        {
+            int mid=low + (high-low)/2;
+
+            long sq= (long) mid * mid;
+
+
+            if(sq <=x)
+            {
+                ans=mid;
+                low=mid+1;
+            }
+
+            else
+            {
+                high=mid-1;
+            }
+        }
+
+        return ans;
+    }
+}
