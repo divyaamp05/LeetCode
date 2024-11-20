9ecf4e270f409aa0fe2b5c70db60b2eecfe83e83
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2024-11-20 14:57:17 +0530

    Time: 5 ms (8.89%), Space: 44.8 MB (5.69%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,17 +1,83 @@
 class Solution {
     public boolean checkIfExist(int[] arr)
     {
-        for(int i=0;i<arr.length;i++)
+        return mergesort(arr,0,arr.length-1);
+    }
+
+    public boolean mergesort(int[] arr,int low,int high)
+    {
+        if(low>=high)
         {
-            for(int j=i+1;j<arr.length;j++)
+            return false;
+        }
+
+        int mid=(low+high)/2;
+
+        boolean left=mergesort(arr,low,mid);
+        boolean right=mergesort(arr,mid+1,high);
+        boolean flag=cnt(arr,low,mid,high);
+        merge(arr,low,mid,high);
+
+        return flag || left || right;
+    }
+
+    public boolean cnt(int[]arr,int low,int mid,int high)
+    {
+
+        for(int i=low;i<=mid;i++)
+        {
+            int right=mid+1;
+            while(right<=high)
             {
-                if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
+                if(arr[i]==2*arr[right] || arr[right]==2*arr[i])
                 {
                     return true;
                 }
+                right++;
             }
-        }
+        } 
 
         return false;
+    }
+
+    public void merge(int[]arr,int low,int mid,int high)
+    {
+        int left=low;
+        int right=mid+1;
+
+        ArrayList <Integer> temp =new ArrayList <> ();
+
+        while(left<=mid && right<=high)
+        {
+            if(arr[left]<=arr[right])
+            {
+                temp.add(arr[left]);
+                left++;
+            }
+
+            else
+            {
+                temp.add(arr[right]);
+                right++;
+            }
+
+        }
+
+        while(left<=mid)
+        {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while(right<=high)
+        {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i=low;i<=high;i++)
+        {
+            arr[i]=temp.get(i-low);
+        }
     }
 }
