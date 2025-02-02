0fc3035bc75e235cfbdc100a0b1bb91bc623cfa5
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-02-02 09:59:18 +0530

    Time: 0 ms (100%), Space: 41.7 MB (38.12%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -1,21 +1,6 @@
 class Solution {
     public boolean checkString(String s) 
     {
-        int n=s.length();
-
-        if(s.charAt(0)=='b' & s.charAt(n-1)=='a')
-        {
-            return false;
-        }
-
-        for(int i=n-1;i>0;i--)
-        {
-            if((s.charAt(i)=='a') && (s.charAt(i-1)=='b'))
-            { 
-                return false;
-            }
-        }
-
-        return true;
+        return !(s.contains("ba"));
     }
 }
