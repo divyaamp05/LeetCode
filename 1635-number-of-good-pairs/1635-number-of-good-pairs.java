d253ecf448c65b816bc906db6acf924f56b8ced3
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-02-10 12:35:45 +0530

    Time: 1 ms (88.24%), Space: 40.9 MB (78.52%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -1,86 +1,19 @@
-class Solution 
-{
+class Solution {
     public int numIdenticalPairs(int[] nums) 
     {
-        return mergesort(nums,0,nums.length-1);
-    }
+        int count=0;
 
-    public int mergesort(int [] nums,int low,int high)
-    {
-        int cnt=0;
-
-        if(low>=high)
+        for(int i=0;i<nums.length;i++)
         {
-            return 0;
-        }
-
-        int mid=(low+high)/2;
-
-        cnt+=mergesort(nums,low,mid);
-        cnt+=mergesort(nums,mid+1,high);
-        cnt+=count(nums,low,mid,high);
-        merge(nums,low,mid,high);
-
-        return cnt;
-    }
-
-    public int count(int [] nums,int low,int mid,int high)
-    {
-        int cnt=0;
-        
-
-        for(int i=low;i<=mid;i++)
-        {
-            int right=mid+1;
-            while(right<=high)
+            for(int j=i+1;j<nums.length;j++)
             {
-                if(nums[i]==nums[right])
+                if(nums[i]==nums[j])
                 {
-                    cnt++;
+                    count++;
                 }
-                right++;
-            } 
-        }
-
-        return cnt;
-    }
-
-    public void merge(int [] nums,int low,int mid,int high)
-    {
-        ArrayList <Integer> temp =new ArrayList <> ();
-
-        int left=low;
-        int right=mid+1;
-
-        while(left<=mid && right <=high)
-        {
-            if(nums[left]<=nums[right])
-            {
-                temp.add(nums[left]);
-                left++;
-            }
-            else
-            {
-                temp.add(nums[right]);
-                right++;
             }
         }
 
-        while(left<=mid)
-        {
-            temp.add(nums[left]);
-            left++;
-        }
-
-        while(right<=high)
-        {
-            temp.add(nums[right]);
-            right++;
-        }
-
-        for(int i=low;i<=high;i++)
-        {
-            nums[i]=temp.get(i-low);
-        }
+        return count;
     }
 }
