5e80e80b7f5c38f55036d143dd2d27865b4d554a
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-02-21 16:15:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) 
+    {
+        int i=0;
+        for(int j=1;j<nums.length;j++)
+        {
+            if(nums[j]!=0 && nums[i]==0)
+            {
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
+            }
+
+            else if(nums[j]==0 && nums[i]!=0)
+            {
+                i++;
+            }
+        }
+    }
+}
