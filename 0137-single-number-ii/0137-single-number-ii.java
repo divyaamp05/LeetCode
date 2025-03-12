31425da4daf4c44635137c69d7839a0fd7990170
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-03-13 00:49:19 +0530

    Time: 4 ms (52.89%), Space: 45.9 MB (19.41%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,33 +1,12 @@
-class Solution 
-{
-    public int singleNumber(int[] nums) 
-    {
+class Solution {
+    public int singleNumber(int[] nums) {
         Arrays.sort(nums);
-
-        int n= nums.length;
-
-        if(n==1)
-        {
-            return nums[0];
-        }
-        else if(nums[0]!=nums[1])
-        {
-            return nums[0];
-        }
-        else if(nums[n-2]!=nums[n-1])
-        {
-            return nums[n-1];
-        }
-
-        for(int i=1;i<nums.length;i+=3)
-        {
-            if(nums[i]!=nums[i-1])
-            {
+        int n=nums.length;
+        for(int i=1;i<n;i+=3){
+            if(nums[i]!=nums[i-1]){
                 return nums[i-1];
             }
         }
-
-        return nums[nums.length-1];
-
+        return nums[n-1];
     }
 }
