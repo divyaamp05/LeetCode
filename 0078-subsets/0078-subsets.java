8f7b1af6f72e7379e4598d74c6c1cda213d82e03
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-03-19 19:51:43 +0530

    Time: 0 ms (100%), Space: 42.9 MB (30.18%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result=new ArrayList <> ();
-
+        List<List<Integer>> result=new ArrayList<> ();
         fun(nums,0,new ArrayList <> (),result);
         return result;
+        
+        
     }
 
-    public void fun(int[] nums,int i,List<Integer> current,List<List<Integer>> result ){
-    if(i>=nums.length){
-        result.add(new ArrayList<>(current));
-        return;
-    }
+    public void fun(int []nums,int i,List <Integer> current,List<List<Integer>> result){
+        if(i>=nums.length){
+            result.add(new ArrayList <> (current));
+            return;
+        }
 
-    current.add(nums[i]);
-    fun(nums,i+1,current,result);
-    current.remove(current.size()-1);
-    fun(nums,i+1,current,result);
-
+        current.add(nums[i]);
+        fun(nums,i+1,current,result);
+        current.remove(current.size()-1);
+        fun(nums,i+1,current,result);
 
     }
 }
