f3cbbf2f9f421f02a78783f861739a3a01e9727b
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-05-16 09:43:08 +0530

    Time: 4 ms (58.97%), Space: 42.1 MB (99.59%) - LeetHub

---
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/3871-minimum-deletions-for-at-most-k-distinct-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDeletion(String s, int k) {
+
+        HashMap<Character,Integer> mp=new HashMap <> ();
+
+        for(char ch:s.toCharArray()){
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+        }
+
+        int a=mp.size();
+
+        if(a<=k){
+            return 0;
+        }
+        ArrayList <Integer> ls=new ArrayList <> (mp.values());
+        Collections.sort(ls);
+        int b=ls.size();
+        int deletions=0;
+        for(int i=0;i<b-k;i++){
+            deletions+=ls.get(i);
+        }
+        
+        return deletions;
+    }
+}
