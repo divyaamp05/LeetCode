76019ec45432cc29c431a5ccac5fe094036fa151
Divya Ampabattuni <136983111+divyaamp05@users.noreply.github.com>
2025-07-06 19:14:59 +0530

    Time: 0 ms (100%), Space: 41.8 MB (30.5%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,14 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char str[]=s.toCharArray();
+        int n=str.length;
+        for(int i=0;i<n;i++){
+            char ch=str[i];
+            if(ch>='A' && ch<='Z'){
+                str[i]=(char)(ch-'A'+'a');
+            }
+        }
+
+        return new String(str);
     }
 }
